db598668e0627f1f9a148e81ee4688da0650984e
Anthony Tossell <atossell91@gmail.com>
2024-09-05 00:27:51 -0700

    e

--- include/EventTypes.h
+++ include/EventTypes.h
@@ -4,5 +4,6 @@ namespace RixinSDL {
     namespace EventTypes {
     // Populate the namespace here
         const int BUFFER_IMAGE_EVENT = 1;
+        const int ADD_SHADER_EVENT = 2;
     } // EventTypes
 }
