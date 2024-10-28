1e1b36e084d9c8b25f4b593b77f0a363fd749e87
Anthony Tossell <atossell91@gmail.com>
2024-10-27 21:39:12 -0700

    events

--- src/SDLEventManager.cpp
+++ src/SDLEventManager.cpp
@@ -16,80 +16,36 @@ void RixinSDL::SDLEventManager::ProcessEvents() {
 }
 
 void SDLEventManager::HandleEvent(SDL_Event& event) {
-    std::cout << "Event type " << event.type << std::endl;
     switch (event.type)
     {
     case SDL_EVENT_WINDOW_CLOSE_REQUESTED:
-        for (auto handler : closeWindowHandlers) {
-            handler(event.window.windowID);
-        }
+        // Event
         break;
     case SDL_EVENT_KEY_DOWN:
-        for (auto handler : keyDownHandlers) {
-            handler(event.key.key);
-        }
+        // Event
         break;
 
     case SDL_EVENT_KEY_UP:
-        for (auto handler : keyUpHandlers) {
-            handler(event.key.key);
-        }
+        // Event
         break;
 
     case SDL_EVENT_MOUSE_MOTION:
-        for (auto handler : this->mouseMoveHandlers) {
-            SDL_MouseMotionEvent ev = *(reinterpret_cast<SDL_MouseMotionEvent*>(&event));
-            handler(ev.x, ev.y);
-        }
+        // Event
         break;
 
     case SDL_EVENT_MOUSE_BUTTON_DOWN:
-        for (auto handler : this->mouseButtonDownHandlers) {
-            //handler();
-        }
+        // Event
         break;
 
     case SDL_EVENT_MOUSE_BUTTON_UP:
-        for (auto handler : this->mouseButtonUpHandlers) {
-            //handler();
-        }
+        // Event
         break;
 
     case SDL_EVENT_QUIT:
-        for (auto handler : this->quitHandlers) {
-            handler();
-        }
+        // Event
         break;
     
     default:
         break;
     }
-}
-
-void SDLEventManager::AddQuitHandler(std::function<void()> handler) {
-    this->quitHandlers.push_back(handler);
-}
-
-void SDLEventManager::AddKeyDownHandler(std::function<void(int)> handler) {
-    keyDownHandlers.push_back(handler);
-}
-
-void SDLEventManager::AddKeyUpHandler(std::function<void(int)> handler) {
-    keyUpHandlers.push_back(handler);
-}
-
-void SDLEventManager::AddMouseButtonDownHandler(std::function<void(int)> handler) {
-    mouseButtonDownHandlers.push_back(handler);
-}
-
-void SDLEventManager::AddMouseButtonUpHandler(std::function<void(int)> handler) {
-    mouseButtonUpHandlers.push_back(handler);
-}
-
-void SDLEventManager::AddMouseMoveHandler(std::function<void(float posX, float posY)> handler) {
-    mouseMoveHandlers.push_back(handler);
-}
-
-void SDLEventManager::AddCloseWindowHandler(std::function<void(int id)> handler) {
-    closeWindowHandlers.push_back(handler);
 }
